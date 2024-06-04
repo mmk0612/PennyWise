d9711d296d724fcd4a772fab67dd9a1dba3d3e67
mmk0612 <mihirmilindkumar_it21a9_62@dtu.ac.in>
2024-06-04 18:39:13 +0530

    title changed

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 /** @type { import("drizzle-kit").Config } */
 export default {
   schema: "./utils/schema.jsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+    url: process.env.NEXT_PUBLIC_NEON_DATABASE_URL,
   },
 };
