ae5bba0b49da614faa914a114e9229a460f801d7
mmk0612 <mihirmilindkumar_it21a9_62@dtu.ac.in>
2024-06-04 18:49:07 +0530

    Expense List Modified for mobile

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -24,15 +24,14 @@ function ExpenseListTable({ expensesList, refreshData }) {
       <div className="grid grid-cols-4 bg-slate-300 p-2">
         <h2 className="font-bold">Name</h2>
         <h2 className="font-bold">Amount</h2>
-        <h2 className="font-bold">Date</h2>
+        <h2 className="font-bold hidden sm:block">Date</h2>
         <h2 className="font-bold">Action</h2>
       </div>
-      {
-      expensesList.map((expenses, index) => (
+      {expensesList.map((expenses, index) => (
         <div className="grid grid-cols-4 bg-slate-50 p-2">
           <h2>{expenses.name}</h2>
           <h2>{expenses.amount}</h2>
-          <h2>{expenses.createdAt}</h2>
+          <h2 className="hidden sm:block">{expenses.createdAt}</h2>
           <h2>
             <TrashIcon
               onClick={() => deleteExpense(expenses)}
