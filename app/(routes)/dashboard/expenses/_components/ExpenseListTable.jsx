9c4853aaedf4143f3d49f001ffe8cf1ea2656ab4
mmk0612 <mihirmilindkumar_it21a9_62@dtu.ac.in>
2024-06-04 19:25:46 +0530

    Expense List layout changed

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -21,16 +21,16 @@ function ExpenseListTable({ expensesList, refreshData }) {
   return (
     <div className="mt-3">
       <h2 className="text-lg font-bold mt-5 mb-5">Your Latest Expenses</h2>
-      <div className="grid grid-cols-5 bg-slate-300 p-2">
+      <div className="grid grid-cols-6 bg-slate-300 p-2">
         <h2 className="font-bold col-span-2">Name</h2>
-        <h2 className="font-bold">Amount</h2>
+        <h2 className="font-bold col-span-2">Amount</h2>
         <h2 className="font-bold hidden sm:block">Date</h2>
         <h2 className="font-bold">Action</h2>
       </div>
       {expensesList.map((expenses, index) => (
-        <div className="grid grid-cols-5 bg-slate-50 p-2">
+        <div className="grid grid-cols-6 bg-slate-50 p-2">
           <h2 className="col-span-2">{expenses.name}</h2>
-          <h2>{expenses.amount}</h2>
+          <h2 className="col-span-2">{expenses.amount}</h2>
           <h2 className="hidden sm:block">{expenses.createdAt}</h2>
           <h2>
             <TrashIcon
