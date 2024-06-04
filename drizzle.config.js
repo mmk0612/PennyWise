51e7d423b176776a0aa497f42516230ed4efab37
mmk0612 <mihirmilindkumar_it21a9_62@dtu.ac.in>
2024-06-04 18:42:12 +0530

    env db changed

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default {
   schema: "./utils/schema.jsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_NEON_DATABASE_URL,
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
   },
 };
